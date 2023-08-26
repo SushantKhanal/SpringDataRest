32a0d0def474f8e2806c81b35bef7ec6f63efda8
SushantKhanal <dreamerking114@gmail.com>
2023-08-26 11:24:46 -0500

    Spring Data REST custom properties

--- src/main/java/com/example/springdatarest/dao/EmployeeRepository.java
+++ src/main/java/com/example/springdatarest/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.example.springdatarest.dao;
 
 import com.example.springdatarest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     //that's it .... n need to write any code LOL!
